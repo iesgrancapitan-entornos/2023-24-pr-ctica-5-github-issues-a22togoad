98bc6637494fae60db3e099dc1ae60257dd310ca
a22togoad <113971768+a22togoad@users.noreply.github.com>
2024-06-11 00:00:08 +0200

    Corrijo issue #1

--- ej1.py
+++ ej1.py
@@ -6,7 +6,7 @@ Fecha: 20/10/2023
 """
 print("Este programa pide 2 números y comprueba si el primero es mayor que el segundo.\n ")
 num1 = int(input("Dime el primer número: "))
-num2 = in(input("Dime el segundo número: "))
+num2 = int(input("Dime el segundo número: "))
 
 if num1 > num2:
     print("El primer número es mayor")
