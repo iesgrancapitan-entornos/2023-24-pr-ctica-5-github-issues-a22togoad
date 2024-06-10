0675179772ff99b59879417624b74f28c69cdc49
a22togoad <113971768+a22togoad@users.noreply.github.com>
2024-06-10 23:33:49 +0200

    a22togoad commit 1

---
+++ ej1.py
@@ -0,0 +1,14 @@
+"""
+Programa que pida dos números e indique si el primero es mayor que el segundo o no.
+
+Autor: Adrián Torres Gómez
+Fecha: 20/10/2023
+"""
+print("Este programa pide 2 números y comprueba si el primero es mayor que el segundo.\n ")
+num1 = int(input("Dime el primer número: "))
+num2 = int(input("Dime el segundo número: "))
+
+if num1 > num2:
+    print("El primer número es mayor")
+else:
+    print("El primer número no es mayor")
